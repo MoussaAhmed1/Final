2b9042435e8d84ec816f84fe3786ba63583d88fb
Mousa Ahmed <moussa.abdelghany@gmail.com>
2025-01-13 09:44:46 +0200

    adding toaster

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import { ToastrService  } from 'ngx-toastr';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,MatRadioModule,MatButtonModule, MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Final';
+
+  constructor(private _toaster:ToastrService){}
+
+  ngOnInit(): void {
+    this._toaster.success('Hello world!', 'Toastr fun!');
+  }
+
 }
